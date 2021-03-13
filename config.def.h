ebb1fd00429d1145c5a5e032de32b76fadf407ca
charlie <chamgoat@gmail.com>
2021-03-13 15:48:45 -0500

    updated config

--- config.def.h
+++ config.def.h
@@ -65,5 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ netspeed_rx, "%sB/s ", "wlp2s0" },
+	{ run_command, " %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ cpu_perc, "[CPU %s%%]	", NULL		},
+	{ ram_perc, "[RAM %s%%]	", NULL		},
+	{ datetime, "%s",           "%a %b %d %r" },
 };
